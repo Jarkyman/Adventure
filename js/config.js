2ad4654e2972f6fb7b6c07ce62c3623e7d36bb9b
vital <vitaliy_serdyukov@hotmail.com>
2022-03-21 11:40:47 +0100

    TX_17_frontpage_design_start

--- js/config.js
+++ js/config.js
@@ -19,3 +19,12 @@ const gray = '#e6e9eb' //rgb(230,233,235)
 const orange = '#e4480b' //rgb(228,72,11)
 const blueGray = '#505a56' //rgb(80,90,86)
 const navy = '#1f415d' //rgb(31,65,93)
+
+const body = document.getElementById('body');
+body.style.backgroundColor = black;
+
+const header = document.getElementById('header');
+header.style.backgroundColor = darkblue;
+
+const footer = document.getElementById('footer');
+footer.style.backgroundColor = darkblue;
