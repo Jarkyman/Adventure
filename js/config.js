66cca264ab6cb9bed7465d9bdb92422b7c727564
jackiejensen <Jackiehartvig@hotmail.com>
2022-03-21 09:32:43 +0100

    added const color in config.js

--- js/config.js
+++ js/config.js
@@ -8,3 +8,14 @@ const baseurl = 'http://localhost:8080/'; //TODO: skal ændres til vores sky
 const createurl = 'create/';
 const updateurl = 'update/';
 const deleteurl = 'delete/';
+
+//Colors
+const lightBlue = '#42b2df' //rgb(66,178,223)
+const yellow = '#edc73d' //rgb(237,199,61)
+const darkblue = '#1f8bca' //rgb(31,139,202)
+const brown = '#8c6338' //rgb(140,99,56)
+const black = '#16100c' //rgb(22,16,12)
+const gray = '#e6e9eb' //rgb(230,233,235)
+const orange = '#e4480b' //rgb(228,72,11)
+const blueGray = '#505a56' //rgb(80,90,86)
+const navy = '#1f415d' //rgb(31,65,93)
