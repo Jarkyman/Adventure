daaef54331d2dda76aca0ce029e1f17ee0b19036
vital <vitaliy_serdyukov@hotmail.com>
2022-03-23 09:47:50 +0100

    TX_17_home_page_plus_styling

--- js/config.js
+++ js/config.js
@@ -10,21 +10,26 @@ const updateurl = 'update/';
 const deleteurl = 'delete/';
 
 //Colors
-const lightBlue = '#42b2df' //rgb(66,178,223)
-const yellow = '#edc73d' //rgb(237,199,61)
-const darkblue = '#1f8bca' //rgb(31,139,202)
-const brown = '#8c6338' //rgb(140,99,56)
-const black = '#16100c' //rgb(22,16,12)
-const gray = '#e6e9eb' //rgb(230,233,235)
-const orange = '#e4480b' //rgb(228,72,11)
-const blueGray = '#505a56' //rgb(80,90,86)
-const navy = '#1f415d' //rgb(31,65,93)
+const lightBlue = '#42b2df'; //rgb(66,178,223)
+const yellow = '#edc73d'; //rgb(237,199,61)
+const darkblue = '#1f8bca'; //rgb(31,139,202)
+const brown = '#8c6338'; //rgb(140,99,56)
+const black = '#16100c'; //rgb(22,16,12)
+const gray = '#fefefe'; //rgb(230,233,235)
+
+
+const orange = '#e4480b'; //rgb(228,72,11)
+const blueGray = '#505a56'; //rgb(80,90,86)
+const navy = '#1f415d'; //rgb(31,65,93)
 
 const body = document.getElementById('body');
-body.style.backgroundColor = black;
+body.style.backgroundColor = gray;
 
 const header = document.getElementById('header');
 header.style.backgroundColor = darkblue;
 
 const footer = document.getElementById('footer');
 footer.style.backgroundColor = darkblue;
+
+/*const links = document.getElementById('li_a');
+footer.style.backgroundColor = yellow;*/
